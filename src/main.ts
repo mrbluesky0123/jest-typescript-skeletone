9eac88a4ac16b61849f28e6df62881981b168a0a
randy.kang <randy.kang@kakaobank.com>
2023-10-30 17:40:07 +0900

    AAA

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import axios from 'axios'
+import type { AxiosResponse, InternalAxiosRequestConfig } from "axios";
+import jsConvert from "js-convert-case"
 let start: string = "string";
 console.log(start); 
 
@@ -8,6 +10,40 @@ console.log(start);
 export const sum = (a: number, b: number): number => a + b;
 
 export const example = async (name: string): Promise<string> => {
-    const result: {count: number, name: string, age: number} = await axios.get(`https://api.agify.io/?name=${name}`)
-    return result.name;
+    axios.interceptors.request.use(<T>(config: InternalAxiosRequestConfig<T>) => {
+        console.log("axios-request-config =>", config);
+
+        // case 변환
+        const convertedData = jsConvert.snakeKeys(config.data);
+        console.log("##!!!!!!## ", convertedData);
+        config.data = convertedData === null? undefined : convertedData as typeof config.data;
+
+        return config;
+    }, (error: any) => {
+        console.log("axios-request-error =>", error);
+
+        return Promise.reject(error);
+    });
+
+    axios.interceptors.response.use(<T, D>(response: AxiosResponse<T, D>) => {
+        console.log("axios-response =>", response);
+
+        // case 변환
+        const convertedData = jsConvert.camelKeys(response.data);
+        console.log("#### ", convertedData);
+        response.data = convertedData as typeof response.data;
+        
+        return response.data;
+    }, (error: any) => {
+        console.log("axios-response-error", error);
+
+        return Promise.reject(error);
+    });
+    const request: {snake_case: string, how_long: number} = {
+        snake_case: "snake_case",
+        how_long: 10
+    }
+    const result: {snake_case: string, how_long: number} = await axios.post(`http://cfe83c5d-4adf-443d-a4bc-aa616c909349.mock.pstmn.io/testapi/post`, request)
+    console.log("#@@@#@#@#@#@#@# ", result);
+    return result.snake_case;
 }
--- src/sum.test.ts
+++ src/sum.test.ts
@@ -2,24 +2,35 @@ import {test, expect} from '@jest/globals'
 import { sum, example } from './main'
 import axios from 'axios'
 
-jest.mock('axios')
+// jest.mock('axios')
 
 describe('test with 2 cases: ', () => {
-    describe('test case 1: ', () => { 
-        it.failing('mocking axios', async () => {
-            (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "aaa", age: 12});
-            (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "qqq", age: 12});
+    // describe('test case 1: ', () => { 
+    //     it.failing('mocking axios', async () => {
+    //         // (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "aaa", age: 12});
+    //         // (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "qqq", age: 12});
+    //         // console.log(a)
+    //         const b = await example("qqq")
+    //         console.log(b)
+    //         expect(b).toBe("aaa");
+    //     });
+    // });
+    // describe('test case 2: ', () => { 
+    //     it.failing('sum test', () => {
+    //         const b = sum(1, 2)
+    //         console.log(b)
+    //         expect(b).toBe(4);
+    //     });
+    // });
+    describe('test case 3: ', () => { 
+        it('axios case conversion', async () => {
+            // (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "aaa", age: 12});
+            // (axios.get as jest.Mock).mockResolvedValue({count: 123, name: "qqq", age: 12});
             // console.log(a)
-            const b = await example("qqq")
+
+            const b = await example("aaaa")
             console.log(b)
-            expect(b).toBe("aaa");
-        });
-    });
-    describe('test case 2: ', () => { 
-        it.failing('sum test', () => {
-            const b = sum(1, 2)
-            console.log(b)
-            expect(b).toBe(4);
+            expect(b).toBe("aaaa");
         });
     });
 });
